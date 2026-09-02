510c41428f9938b562b7b0825439f2bd019365fc
MuhFakhriIhsan <muhfakhriihsan2026@gmail.com>
2026-09-02 22:12:07 +0800

    Day01.java

---
+++ Day01.java
@@ -0,0 +1,15 @@
+public class Main  {
+	// ini untuk yang 1 kalimat (day 1 belajar ngoding)
+	
+	/* ini untuk yang menurun
+	* seperti ini
+	* dan ini
+	*/
+	
+	public static void main(String[] args)  {
+		
+		System.out.print("hello world");
+		
+	}
+	
+}
