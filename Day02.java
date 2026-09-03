64c5ed253a865a3cda794f510bb7e5de926d48c8
MuhFakhriIhsan <muhfakhriihsan2026@gmail.com>
2026-09-03 20:45:49 +0800

    Day02.java

---
+++ Day02.java
@@ -0,0 +1,16 @@
+public class Main{
+	public static void main(String[] args){
+		
+		String nama = ("Fahri");
+		int umur = 19;
+		String kelamin = ("Laki-Laki");
+		
+		System.out.printf("Nama saya: %s \n",nama);
+		System.out.println("Umur saya : " + umur);
+		System.out.println("jenis kelamin : " + kelamin);
+		
+		System.out.print("saya alumni sman 1 wonomulyo dan hobi saya bermain emel");
+		
+	}
+	
+}
