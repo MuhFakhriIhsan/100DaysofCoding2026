7b6919f9c7b8f999c5461cbd782d49d740d7552b
MuhFakhriIhsan <muhfakhriihsan2026@gmail.com>
2026-09-05 20:49:17 +0800

    wDay04.java

---
+++ Day04.java
@@ -0,0 +1,30 @@
+public class Main{
+	public static void main(String[] args){
+		
+	
+	// byte : untuk data ukuran kecil (8 bit)
+	
+		byte umur = 20;
+			
+	// short : untuk efisiensi memory (16 bit)
+	
+		short tahunlahir  = 2007;
+		
+	// int : untuk angka umum karena paling efisiensi (32 bit)
+	
+		int nik = 123456789;
+		
+	// long : untuk menampung angka diluar kapasitas int dan di akhiri huruf 'L' dibelakang nilai
+		
+		long noheadphone = 6285297700167L;
+		
+		
+			System.out.println("umur : " + umur + " tahun" );
+			System.out.println("tahun lahir : " + tahunlahir);
+			System.out.println("Nik : " + nik);
+			System.out.println("no. HP : " + noheadphone);
+	
+		
+	}
+	
+}
