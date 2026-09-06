cb394c8a1983cb6a9a64fe3edc8d24ab0b4eb138
MuhFakhriIhsan <muhfakhriihsan2026@gmail.com>
2026-09-06 22:39:42 +0800

    Day05.java

---
+++ Day05.java
@@ -0,0 +1,14 @@
+public class Main {
+	public static void main(String[] args) {
+	
+		//float adalah tipe data pecahan dengan presisi tunggal dan penulisannya didalam kode harus diakhiri huruf (f).
+		
+		//double adalah tipe data pecahan dengan presisi ganda dan tanpa menggunakan akhiran huruf.
+			
+		float beratbadan = 50.9f;
+		double tinggibadan = 168.81;
+		
+		System.out.println("Berat badan saya : " + beratbadan + " kg");
+		System.out.println("Tinggi badan saya : " + tinggibadan + " cm");
+	}
+}
