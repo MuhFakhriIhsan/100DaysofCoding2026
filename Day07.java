5e4138ecd4fcc65a4d9187dda304b3234a9863c3
MuhFakhriIhsan <muhfakhriihsan2026@gmail.com>
2026-09-08 19:47:17 +0800

    Day07.java

---
+++ Day07.java
@@ -0,0 +1,14 @@
+public class Main {
+	public static void main(String[] args) {
+		
+		//String merupakan bentuk data dalam pemrograman yang dipakai untuk menyimpan serta mengolah teks.
+		
+		String nama = ("fahri");
+		String nim = ("D022");
+		String prodi = ("Informatika");
+		
+		System.out.println("Hai, nama saya " + nama);
+		System.out.println("Saya merupakan mahasiswa baru fakultas teknik dengan nim " +nim);
+		System.out.println("Saya berasal dari prodi teknik " + prodi);
+	}
+}
