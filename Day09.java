dc8a1fb829f119c04f024986669f3597640c0082
MuhFakhriIhsan <muhfakhriihsan2026@gmail.com>
2026-09-10 19:56:30 +0800

    Day09.java

---
+++ Day09.java
@@ -0,0 +1,18 @@
+public class Main{
+	public static void main(String[] args){
+		
+		// Mengunci data string dan int menggunakan final
+		
+		final String namaMahasiswa = "FAHRI";
+		final String nim = "D022";
+		final int pinAkun = 1234;
+		
+		System.out.println("Nama Mahasiswa : " + namaMahasiswa);
+		System.out.println("Nim : " + nim);
+		System.out.println("Sandi akun : " + pinAkun);
+		
+		
+		
+	}
+	
+}
