1494504c22e51db98fe2ef0ac805156c3fceb88e
MuhFakhriIhsan <muhfakhriihsan2026@gmail.com>
2026-09-11 20:07:51 +0800

    Day10.java

---
+++ Day10.java
@@ -0,0 +1,31 @@
+public class Main{
+	public static void main(String[] args){
+		
+		// BATAS MINIMAL DAN MAKSIMAL TIPE DATA
+		
+		/* Tipe data :
+		*Byte
+		*Short
+		*Int
+		*Long
+		*Float
+		*Double
+		*Char
+		*/
+		
+		System.out.println("Batas Byte : " + Byte.MIN_VALUE + " Sampai " + Byte.MAX_VALUE);
+		
+		System.out.println("Batas Short : " + Short.MIN_VALUE + " Sampai " + Short.MAX_VALUE);
+		
+		System.out.println("Batas Integer : " + Integer.MIN_VALUE + " Sampai " + Integer.MAX_VALUE);
+		
+		System.out.println("Batas Long : " + Long.MIN_VALUE + " Sampai " + Long.MAX_VALUE);
+		
+		System.out.println("Batas Float : " + Float.MIN_VALUE + " Sampai " + Float.MAX_VALUE);
+		
+		System.out.println("Batas Double : " + Double.MIN_VALUE + " Sampai " + Double.MAX_VALUE);
+		
+		System.out.println("Batas Char : " + (int) Character.MIN_VALUE + " Sampai " + (int) Character.MAX_VALUE);
+	}
+	
+}
