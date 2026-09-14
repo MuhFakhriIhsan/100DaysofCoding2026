88776dadfec7604962a405525c7feda711b68549
MuhFakhriIhsan <muhfakhriihsan2026@gmail.com>
2026-09-14 16:49:32 +0800

    Day13.java

---
+++ Day13.java
@@ -0,0 +1,10 @@
+public class Main {
+	public static void main(String[] args) {
+		
+		double angka1 = 2.56;
+		double angka2 = 3.14;
+		
+		System.out.printf("angka 1 : %.2f %n " , angka1);
+		System.out.printf("angka 2 : %.2f %n " , angka2);
+	}
+}
