3ed336dfb1b502588781321fba19d866317d5321
MuhFakhriIhsan <muhfakhriihsan2026@gmail.com>
2026-09-15 22:22:38 +0800

    Day14.java

---
+++ Day14.java
@@ -0,0 +1,11 @@
+public class Main {
+	public static void main(String[] args) {
+		int A = 8;
+		int B = 15;
+		int jumlah = A+B;
+		int kurang = A-B;
+		
+		System.out.println("Hasil penjumlahan = " + jumlah);
+		System.out.println("Hasil pengurangan = " + kurang);
+	}
+}
