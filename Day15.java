b31801697365d6a12d9169e7820c91b4abfffcba
MuhFakhriIhsan <muhfakhriihsan2026@gmail.com>
2026-09-16 20:55:09 +0800

    Day15.java

---
+++ Day15.java
@@ -0,0 +1,13 @@
+public class Main {
+	public static void main(String[] args) {
+		
+		int A = 8;
+		int B = 2;
+		
+		int kali = A*B;
+		int bagi = A/B;
+		
+		System.out.println("Hasil perkalian = " + kali);
+		System.out.println("Hasil pembagian = " + bagi);
+	}
+}
