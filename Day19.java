9e1f42fc6272347afc09bc11fd3fa013249a0bf4
MuhFakhriIhsan <muhfakhriihsan2026@gmail.com>
2026-09-20 13:31:14 +0800

    Day19.java

---
+++ Day19.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Day19 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+        System.out.print("Nilai double : ");
+        double A = in.nextDouble();
+        int B = (int) A;
+
+        System.out.printf("Nilai int : %d " , B);
+    }
+}
