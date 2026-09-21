f80f4e53e98deb25dff89fad5d70669103f12c33
MuhFakhriIhsan <muhfakhriihsan2026@gmail.com>
2026-09-21 12:25:25 +0800

    Day21.java

---
+++ Day21.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Day21 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        
+        System.out.print("Masukkan angka: ");
+        String Teks = in.nextLine(); 
+        
+        int angka = Integer.parseInt(Teks);
+        
+        System.out.println("Nilai int : " + angka);
+        
+        }
+      
+    }
