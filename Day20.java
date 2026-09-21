9840fe356a733b606d10fbf0ca57415ef4ae9df6
MuhFakhriIhsan <muhfakhriihsan2026@gmail.com>
2026-09-21 12:24:33 +0800

    Day20.java

---
+++ Day20.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Day20{
+        public static void main(String[] args) {
+            Scanner in =new Scanner(System.in);
+
+            System.out.print("Nilai int :  ");
+        int angka = in.nextInt();
+
+        String teks = String.valueOf(angka);
+
+        System.out.println("Nilai string : " + teks);
+        
+        }
+    }
