8e92071167f342c677ef3b4246f6b8130cbbef0a
MuhFakhriIhsan <muhfakhriihsan2026@gmail.com>
2026-09-23 21:00:44 +0800

    Day23.java

--- Day23.java
+++ Day23.java
@@ -4,11 +4,11 @@ public class Day23 {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
 
-        System.out.print("Sisi : ");
+        System.out.print("Masukkan sisi persegi : ");
         int sisi = in.nextInt();
 
         int kali = sisi*sisi;
 
-        System.out.printf("Luas persegi :%d " , kali);
+        System.out.printf("Luas persegi :%d cm\u00B2 " , kali);
     }
 }
